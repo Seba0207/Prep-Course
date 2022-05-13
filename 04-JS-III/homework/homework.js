bc3203bee80f1db40cd887d06fbe0c30ec164dae
Sebastian <seba.a92@hotmail.com>
2022-05-13 15:00:42 -0300

    homework III

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -105,7 +105,7 @@ function numeroMasGrande(numeros) {
   var maximo = numeros[0];
  for(var i = 1; i < numeros.length;i++){
   if(numeros[i] > maximo){
-    maximo = numeros [i];
+    maximo = numeros[i];
   }
 }
   return maximo;
@@ -115,13 +115,25 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if(arguments.length < 1) return 0;
+  var total = 1;
+  for(var i = 0; i < arguments.length; i++){
+    total = total * arguments[i];
+  }
+  return total;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var contador = 0;
+  for(var i = 0; i < arreglo.length; i++){
+    if(arreglo[i] > 19){
+      contador++
+    }
+  }
+  return contador
 }
 
 
