25d75f0407b8f5ddc2d1ee3750d88a434c30095b
Sebastian <seba.a92@hotmail.com>
2022-05-13 19:31:14 -0300

    homework III

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -142,7 +142,10 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if( numeroDeDia === 1 || numeroDeDia === 7){
+    return 'Es fin de semana';
+  }
+    return 'Es dia Laboral';
 } 
 
 
@@ -150,6 +153,11 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  let num = n.toString()
+  if(num.charAt(0) === "9"){
+    return true
+  }
+  return false
   
 }
 
@@ -157,8 +165,13 @@ function empiezaConNueve(n) {
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí  
-  
+  //Escribe tu código aquí
+  for(var i =0; i< arreglo.length - 1; i++){
+    if(arreglo[i] !== arreglo[i+1]){
+      return false
+    }
+  }  
+  return true
 } 
 
 
@@ -167,13 +180,33 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
-}
+  var nuevoArray = [];
 
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === 'Enero' ||array[i] === 'Marzo' || array[i] === 'Noviembre'){
+      nuevoArray.push[i]
+    }
+  }
+    if(nuevoArray.length < 3){
+      return "No se encontraron los meses perdidos";
+    }
+    else {
+      return nuevoArray;
+    }
+}
 
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var nuevoArray = [];
+
+  for(let i = 0; i < array.length; i++){
+    if(array[i] > 100){
+      nuevoArray.push(array[i]);
+    }
+  }
+  return nuevoArray;
 }
 
 
